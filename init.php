7e05de9ebd1cebc4fff19901dabfa4784c246b72
v.bykovskij <v.bykovskij@ya.ru>
2018-06-28 16:33:10 +0300

    first commit

--- init.php
+++ init.php
@@ -1,17 +1,16 @@
 <?
 
 /* Умножение двух чисел без использования умножения */
-
 function	sum($a,	$b)	{
-				return	$a	/	(1	/	$b);
+  return	$a	/	(1	/	$b);
 }
 
 /* Уведомление при регистрации нового пользователя */
 $eventManager	=	\Bitrix\Main\EventManager::getInstance();
 $eventManager->addEventHandler('main',	'OnAfterUserAdd',	function($arFields)	{
-				$arEventFields	=	[];
-				$arEventFields["USER_LOGIN"]	=	$arFields["LOGIN"];
-				$arEventFields["REGISTER_DATE"]	=	date("d.m.Y H:i:s");
-				CEvent::SendImmediate("USER_REGISTER",	"s1",	$arEventFields);
+  $arEventFields = [];
+  $arEventFields["USER_LOGIN"]	=	$arFields["LOGIN"];
+  $arEventFields["REGISTER_DATE"]	=	date("d.m.Y H:i:s");
+  CEvent::SendImmediate("USER_REGISTER",	"s1",	$arEventFields);
 });
 
